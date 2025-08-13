bea82c74f7d0d18295b5ecd7aac313ccc73a36d9
Kedar Vyas <kedar.vyas@thegatewaycorp.co.in>
2025-08-13 16:40:44 +0530

    feat: add CORS support and update dev script to use cross-env

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,11 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use((req, res, next) => {
